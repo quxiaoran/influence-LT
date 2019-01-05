c37bc7d72293071b5f26756e21ac30a220c17679
Steven Qu <quxiaoran.sq@gmail.com>
2019-01-05 18:40:05 -0500

    Create Testing.java

---
+++ tests/Testing.java
@@ -0,0 +1,15 @@
+package tests;
+
+import java.io.IOException;
+
+public class Testing {
+
+	public static void main(String[] args) throws IOException {
+		
+		GenerateNetwork.main(null);
+		Predictive.main(null);
+		ImpactHeuristic.main(null);
+		
+	}
+
+}
